270e81ab4fe1587d528b1129118e416b6275f46f
Alexandre Oliveira <Alexandre2001.oliveira@gmail.com>
2023-09-25 21:05:31 -0300

    Connecting to the database

--- app.js
+++ app.js
@@ -10,4 +10,12 @@ app.get('/', (req, res) => {
     res.status(200).json({msg: "Bem vindo a API"})
 })
 
-app.listen(3000)
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASS
+
+
+mongosse.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.zuv6swt.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp`).then(() => {
+    app.listen(3000)
+    console.log('Connect database')
+}).catch((err) => console.log(err))
+
