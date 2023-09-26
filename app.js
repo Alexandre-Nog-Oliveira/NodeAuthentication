e4f8a6c9dd2f656b2084e444ba5133519f533cb5
Alexandre Oliveira <Alexandre2001.oliveira@gmail.com>
2023-09-25 21:35:55 -0300

    Creating the user registration route

--- app.js
+++ app.js
@@ -5,9 +5,31 @@ const mongosse = require('mongoose')
 const bcrypt = require('bcrypt')
 
 const app = express()
+app.use(express.json())
+
+const User = require('./models/User')
 
 app.get('/', (req, res) => {
     res.status(200).json({msg: "Bem vindo a API"})
+})
+
+
+app.post('/user/register', async (req, res) => {
+
+    const { name, email, password, confirmpassword } = req.body
+
+    if(!name || !email || !password ){
+
+        return res.status(422).json({message: "Missing field, check if you left any fields blank"})
+
+    } else if(password !== confirmpassword){
+
+        return res.status(422).json({message: "Passwords don't match"})
+        
+    }
+
+    return res.status(200).json({message: "Registered user"})
+
 })
 
 const dbUser = process.env.DB_USER
@@ -18,4 +40,3 @@ mongosse.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.zuv6swt.mongodb
     app.listen(3000)
     console.log('Connect database')
 }).catch((err) => console.log(err))
-
